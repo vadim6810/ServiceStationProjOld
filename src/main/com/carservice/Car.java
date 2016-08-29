99b33f6844663a617b28bdce8fb502a0ba00a9ca
Legat <legatofseal@gmail.com>
2016-08-29 10:43:56 +0300

    Some changhes

--- src/main/com/carservice/Car.java
+++ src/main/com/carservice/Car.java
@@ -4,4 +4,9 @@ package com.carservice;
  * Created by Legat on 8/28/2016.
  */
 public class Car {
+    Integer id;
+    String typeVehicle;
+    CarManufacture carManufacture;
+    CarModel carModel;
+
 }
---
+++ src/main/com/carservice/CarModel.java
@@ -0,0 +1,8 @@
+package com.carservice;
+
+/**
+ * Created by Legat on 8/29/2016.
+ */
+public class CarModel {
+    String model;
+}
