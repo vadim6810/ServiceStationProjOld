444c6d2d039b60483ae3aa99f45623e21e26cc10
Paulo.berezini <paulo.berezini@gmail.com>
2016-08-30 21:30:48 +0300

    Add files via upload

--- src/main/com/carservice/tender/Tender.java
+++ src/main/com/carservice/tender/Tender.java
@@ -3,6 +3,8 @@ package com.carservice.tender;
 import com.carservice.vehicle.CarManufacture;
 import com.carservice.vehicle.TypeVehicle;
 
+import java.lang.String;
+
 /**
  * Created by Legat on 8/29/2016.
  */
@@ -10,7 +12,18 @@ public class Tender {
     Integer id;
     Integer idClient;
     String tenderDescription;
+    Date date;
     TypeVehicle typeVehicle;
     CarManufacture carManufacture;
     boolean tenderState;
+}
+
+public List<Tender> getTenderByClient(int idClient){
+    return idClient;
+}
+public List<Tender> getTenderForContractor(String rangeWork, String typeVehicle,String carManufacture){
+    return idClient;
+}
+public void addTender(){
+
 }
