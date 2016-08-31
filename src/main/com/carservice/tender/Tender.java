166fe9632819b4983639cb09b11af89b47c75e8a
Paulo.berezini <paulo.berezini@gmail.com>
2016-08-31 14:42:04 +0300

    1. Insert Date 2. Create constructor

--- src/main/com/carservice/tender/Tender.java
+++ src/main/com/carservice/tender/Tender.java
@@ -4,6 +4,7 @@ import com.carservice.vehicle.CarManufacture;
 import com.carservice.vehicle.TypeVehicle;
 
 import java.lang.String;
+import java.util.Date;
 
 /**
  * Created by Legat on 8/29/2016.
@@ -13,9 +14,20 @@ public class Tender {
     Integer idClient;
     String tenderDescription;
     Date date;
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     String typeVehicle;
     CarManufacture carManufacture;
     boolean tenderState;
+
+    public Tender(CarManufacture carManufacture, Date date, Integer id, Integer idClient, String tenderDescription, boolean tenderState, String typeVehicle) {
+        this.carManufacture = carManufacture;
+        this.date = new Date(dateFormat.format(date));
+        this.id = id++;
+        this.idClient = idClient;
+        this.tenderDescription = tenderDescription;
+        this.tenderState = tenderState;
+        this.typeVehicle = typeVehicle;
+    }
 }
 
     public List<Tender> getTenderByClient(int idClient){
